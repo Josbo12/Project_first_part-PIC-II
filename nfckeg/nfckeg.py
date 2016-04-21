5698494349232cefcfa7100095f4cd565644772e
erikinho92 <ericdelsams@gmail.com>
2016-04-21 19:14:28 +0200

    database

--- nfckeg/nfckeg.py
+++ nfckeg/nfckeg.py
@@ -1,93 +1,80 @@
-from commandlist import CommandList
+#from commandlist import CommandList
 
-import sensor as sen
-import channels as ch
+import database as db
+
+#import channels as ch
 #import actions as ac
-import time
-import yaml
-import json
+import sys
+#import time
+#import yaml
+#import json
+
+user = sys.argv[1]
+nfc = sys.argv[2]
+username = sys.argv[3]
+
+def __init__(self):
+    #
+    self.database =[]
+    self.database.append(db.Database())
+    for b in self.database:
+
+        b.save_data(user, nfc, username)
 
 
 
-class nfckeg(object):
+    a=0
+    if a==1:
 
 
-    def __init__(self):
-        super(nfckeg, self).__init__()
-        self.cl = CommandList()
-
-         self._get_config()
-         self.channels = []
-
-         self.channels.append(ch.TelegramChannel())
-#         self.actions = []
-#         self.actions.append(ac.MusicPlayer())
-
-    def _get_config(self):
-        with open("nfckeg.yaml") as f:
-            self.cfg = yaml.load(f)
-
-        print "Configuracio: "
-        print json.dumps(self.cfg, indent=4)
+        class nfckeg(object):
 
 
-    def next_command(self):
-        try:
-            return self.cl.next()
-        except:
-            return (None, None)
+            def __init__(self):
+                 super(nfckeg, self).__init__()
+                 self.cl = CommandList()
+
+                 self._get_config()
+                 self.channels = []
+
+                 self.channels.append(ch.TelegramChannel())
+        #         self.actions = []
+        #         self.actions.append(ac.MusicPlayer())
+
+            def _get_config(self):
+                with open("nfckeg.yaml") as f:
+                    self.cfg = yaml.load(f)
+
+                print "Configuracio: "
+                print json.dumps(self.cfg, indent=4)
 
 
-    def update_channels(self):
-        for chan in self.channels:
-            while chan.msg_avail():
-                self.cl.append((chan, chan.get_msg()))
+            def next_command(self):
+                try:
+                    return self.cl.next()
+                except:
+                    return (None, None)
 
 
-    def execute_command(self, command):
-        print "Will execute", command
-        # Foreach Action in actions.
-        #   if is_for_you()
-        #       action.do
-        words = command.split()
-        first_word = words[0]
-        rest_words = words[1:]
-        response = None
-        for a in self.actions:
-            if a.is_for_you(first_word):
-                response = a.do(rest_words)
-                break
-        else:
-            print "No t'entenc"
-        return response
+            def update_channels(self):
+                for chan in self.channels:
+                    while chan.msg_avail():
+                        self.cl.append((chan, chan.get_msg()))
 
 
-    def mainloop(self):
-        # While True:
-        #   command = get_command
-        #   do_command(command)
-        #   update
-
-        target_detected = True
-        while target_detected:
-            #detectem una targeta
-            for s in self.sensors:
-                
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-if __name__ == "__main__":
-    print "Start drinking"
-    NFC = nfckeg()
-    NFC.mainloop()
+            def execute_command(self, command):
+                print "Will execute", command
+                # Foreach Action in actions.
+                #   if is_for_you()
+                #       action.do
+                words = command.split()
+                first_word = words[0]
+                rest_words = words[1:]
+                response = None
+                for a in self.actions:
+                    if a.is_for_you(first_word):
+                        response = a.do(rest_words)
+                        break
+                else:
+                    print "No t'entenc"
+                return response
