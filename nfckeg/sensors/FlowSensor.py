4beaa7c3b96b4344cdfbac8a2139794736fab9b7
Josbo12 <josepborras8@gmail.com>
2016-04-23 15:58:46 +0200

    si existeix el nfc fes la lectura del sensor de flux

--- nfckeg/sensors/FlowSensor.py
+++ nfckeg/sensors/FlowSensor.py
@@ -1,29 +1,37 @@
 import time
 
+from NfcSensor import Nfcsensor as Nf
+
 class FlowSensor():
 
 
     def __init__(self, name="FlowSensor"):
         super(FlowSensor, self).__init__(name)
+        self.Nfcsensor=[]
+        self.Nfcsensor.append(NfcSensor())
 
 
-    def setup():
 
-        read_value=True
-        delay = S
+
+    def setup(self, nfc):
+
+        for a in self.Nfcsensor:
+            a.setup(nfc)
+
+        delay = 5
         self.values=list()
         quantity_flow=0
-        while read_value:
-            quantity_flow = sensor #guarda les dades del sensor en una variable
+
+        while self.nfc == "NFC OK":
+            quantity_flow = 20 #sensor  #guarda les dades del sensor en una variable
             self.values.append(quantity_flow)
-            time.sleep(S)
+            time.sleep(5)
 
     def get_data():
-        return self.values
-
+        print self.values
 
     def  get_cumulative():
         pass
 
     def reset_cumulative()
-        reset sensor
+        #reset sensor
