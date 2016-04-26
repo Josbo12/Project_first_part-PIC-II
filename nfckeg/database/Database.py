13f82444e784bd875eea66910d17158fec2623ba
Josbo12 <josepborras8@gmail.com>
2016-04-26 19:09:39 +0200

    guarda be fent els for

--- nfckeg/database/Database.py
+++ nfckeg/database/Database.py
@@ -4,28 +4,41 @@ from Action import Action
 
 
 class Database(Action):
-    print "eiii"
+
 
     def __init__(self, cfg=None, name="Database"):
          super(Database, self).__init__(cfg, name)
 
     def save_data(self, cfg=None):
 
-        conn = sqlite3.connect('nfcdatabase.db')
+        conn = sqlite3.connect('/home/josep/Project_first_part-PIC-II/database.db')
         cursor = conn.execute("SELECT user, nfc, username from users")
-        user = self.cfg["database"]["user"]
-        nfc = self.cfg["database"]["nfc"]
-        username = self.cfg["database"]["username"]
-        conn.execute('insert into users (user,nfc,username)' +
-                    'values (?, ?, ?);',
-                    (user, nfc, username))
+        users = self.cfg["database"]["user"]
+        for user in users:
+            for usn in user:
+                username = user[usn]['username']
+                nfc = user[usn]['nfc']
+                print usn, username, nfc
+                conn.execute('insert into users (user,nfc,username)' +
+                            'values (?, ?, ?);',
+                            (usn, nfc, username))
+                
+
+
+        #user = user.strip()
+        #nfc = self.cfg["database"]["nfc"]
+        #username = self.cfg["database"]["username"]
+        #conn.execute('insert into users (user,nfc,username)' +
+        #            'values (?, ?, ?);',
+        #            (user, nfc, username))
+        #print ">%s<" %(nfc)
         conn.commit()
         conn.close()
 
 
 
     def get_data(self):
-        conn = sqlite3.connect('nfcdatabase.db')
+        conn = sqlite3.connect('/home/josep/Project_first_part-PIC-II/database.db')
         cursor = conn.execute("SELECT user,nfc,username from users")
         data = [row for row in cursor]
         print data
