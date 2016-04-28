99e650f6fe1d36f6b8994f4581a8bf1803e7e8de
Josbo12 <josepborras8@gmail.com>
2016-04-28 17:52:04 +0200

    fer que els usuaris no es repeteixin

--- nfckeg/database/Database.py
+++ nfckeg/database/Database.py
@@ -12,18 +12,26 @@ class Database(Action):
     def save_data(self, cfg=None):
 
         conn = sqlite3.connect('/home/josep/Project_first_part-PIC-II/database.db')
-        cursor = conn.execute("SELECT user, nfc, username from users")
+
+
+
         users = self.cfg["database"]["user"]
         for user in users:
             for usn in user:
                 username = user[usn]['username']
                 nfc = user[usn]['nfc']
-                print usn, username, nfc
-                conn.execute('insert into users (user,nfc,username)' +
-                            'values (?, ?, ?);',
-                            (usn, nfc, username))
-                
 
+                cursor = conn.execute("SELECT user, nfc, username from users where  user=? and nfc=? and username=?" ,(usn,nfc,username))
+                t=cursor.fetchone()
+                if t == None:
+                    print "USUARIS AFEGITS:"
+                    print usn, username, nfc
+                    conn.execute('insert into users (user,nfc,username)' +
+                                'values (?, ?, ?);',
+                                (usn, nfc, username))
+                else:
+                    print "USUARIS NO AFEGITS"
+                    print usn, username, nfc
 
         #user = user.strip()
         #nfc = self.cfg["database"]["nfc"]
@@ -41,6 +49,6 @@ class Database(Action):
         conn = sqlite3.connect('/home/josep/Project_first_part-PIC-II/database.db')
         cursor = conn.execute("SELECT user,nfc,username from users")
         data = [row for row in cursor]
-        print data
+        #print data
         conn.close()
         return data
