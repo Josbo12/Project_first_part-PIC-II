9d8a23880d0eaa38e6bf3a703906f150e6d077a2
Josbo12 <josepborras8@gmail.com>
2016-04-28 17:53:49 +0200

    funcions per telegram

--- nfckeg/nfckeg.py
+++ nfckeg/nfckeg.py
@@ -3,11 +3,13 @@
 
 import database as db
 import sensors as sen
-#import channels as ch
-#import actions as ac
+import channels as ch
+from commandlist import CommandList
+import actions as ac
 import sys
 import yaml
 import json
+import time
 #import time
 #import yaml
 #import json
@@ -22,14 +24,20 @@ class nfckeg(object):
              super(nfckeg, self).__init__()
 
              #self.save_data(user, nfc, username)
-
+             self.cl = CommandList()
              #self.save_data(user, nfc, username)
              self._get_config()
              self.database = []
              self.database.append(db.Database(self.cfg))
+             self.channels = []
+             #c = ch.TextChannel(self.cfg)
+             #self.channels.append(c)
+             self.channels.append(ch.TelegramChannel(self.cfgtel))
 
              self.sensors = []
              self.sensors.append(sen.NfcSensor())
+             self.actions = []
+             self.actions.append(ac.MusicPlayer())
              #self.sensors.append(sen.FlowSensor(tagID))
              #s
              #self.database.append(db.Database())
@@ -49,9 +57,52 @@ class nfckeg(object):
         def _get_config(self):
             with open("nfckeg.yaml") as f:
                 self.cfg = yaml.load(f)
+            with open ("telegram.yaml") as f1:
+                self.cfgtel = yaml.load(f1)
             #print "Configuracio: "
             #print json.dumps(self.cfg, indent=4)
              #return response
+        def next_command(self):
+            try:
+                return self.cl.next()
+            except:
+                return (None, None)
+
+        def update_channels(self):
+            for chan in self.channels:
+                while chan.msg_avail():
+                    self.cl.append((chan, chan.get_msg()))
+        def execute_command(self, command):
+            print "Will execute", command
+            # Foreach Action in actions.
+            #   if is_for_you()
+            #       action.do
+            words = command.split()
+            print words
+            first_word = words[0]
+            rest_words = words[1:]
+            response = None
+            for a in self.actions:
+                if a.is_for_you(first_word):
+                    response = a.do(rest_words)
+                    break
+            else:
+                print "No t'entenc"
+            return response
+
+        def mainloop(self):
+            # While True:
+            #   command = get_command
+            #   do_command(command)
+            #   update
+            while True:
+                chan, command = self.next_command()
+                if command:
+                    response = self.execute_command(command)
+                    chan.respond(response)
+
+                time.sleep(1)
+                self.update_channels()
 
 
 
@@ -101,7 +152,7 @@ if a==1:
                 words = command.split()
                 first_word = words[0]
                 rest_words = words[1:]
-                response = None
+                response = "hola feo"
                 for a in self.actions:
                     if a.is_for_you(first_word):
                         response = a.do(rest_words)
