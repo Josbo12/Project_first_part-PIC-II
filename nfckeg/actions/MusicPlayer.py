a1afda429fdc57cb13c5f33755b940399a5b96ad
Josbo12 <josepborras8@gmail.com>
2016-04-29 20:09:55 +0200

    passo un usuari i em troba el id

--- nfckeg/actions/MusicPlayer.py
+++ nfckeg/actions/MusicPlayer.py
@@ -16,23 +16,54 @@ class MusicPlayer(Action):
 
 
     def is_for_you(self,user):
-        conn = sqlite3.connect('/home/josep/Project_first_part-PIC-II/database.db')
-        cursor = conn.execute("SELECT user from users where user=?",(user,) )
+        conn1 = sqlite3.connect('/home/josep/Project_first_part-PIC-II/database.db')
+        cursor1 = conn1.execute("SELECT user,nfc from users where user=?",(user,) )
+        t=cursor1.fetchone()
+        data = [row for row in cursor1]
+        print "AIXO ES DATA"
+        print data
+        iden=data[0]
+        for a in iden:
+            print a
+            x=a
+
+        print "aixo es x"
+        print x
+        print "AIXO ES iden"
+
+        print iden
+
+
+
+
+        #conn = sqlite3.connect('/home/josep/Project_first_part-PIC-II/llistabeguts.db')
+        #cursor = conn.execute("SELECT id, quantitatbeguda from beguts where id=?",(self.data2,))
+        #self.data = [row for row in cursor]
+        #print "aixo es data"
+        #print self.data
+
+
+
         #cursor = conn.execute("SELECT username, email from users where (username,email) values (?,?)",username, email )
         #t = [e for e in cursor]
-        t=cursor.fetchone()
+
 
         print "printejo t = ", t
-        conn.close()
+
         if t != None:
-                return True
+            self.data = [row for row in cursor1]
+            print "OK"
+
+            return True
         else:
                 return False
+        conn.close()
 
     def do(self, command):
-        print "Will play music ", " ".join(command)
+        print "Will play music  ", " ".join(command)
         return "OK"
 
+#######################################################################
     def is_for_yo4u(self, word):
         if word in self.hola:
             return True
