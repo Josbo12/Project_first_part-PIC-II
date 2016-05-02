c3fb4cbf830b09e25f9a8290d001ced116217b76
Josbo12 <josepborras8@gmail.com>
2016-05-02 14:21:13 +0200

    nomes autoritzats poden preguntar al BeerBoot

--- nfckeg/actions/MusicPlayer.py
+++ nfckeg/actions/MusicPlayer.py
@@ -1,5 +1,7 @@
 from Action import Action
 import sqlite3
+from sensors import FlowSensor
+import time
 
 class MusicPlayer(Action):
     """MusicPlayer for Ambrosio"""
@@ -16,52 +18,62 @@ class MusicPlayer(Action):
 
 
     def is_for_you(self,user):
+
+
         conn1 = sqlite3.connect('/home/josep/Project_first_part-PIC-II/database.db')
         cursor1 = conn1.execute("SELECT user,nfc from users where user=?",(user,) )
         t=cursor1.fetchone()
-        data = [row for row in cursor1]
-        print "AIXO ES DATA"
-        print data
-        iden=data[0]
-        for a in iden:
-            print a
-            x=a
-
-        print "aixo es x"
-        print x
-        print "AIXO ES iden"
-
-        print iden
-
-
-
-
-        #conn = sqlite3.connect('/home/josep/Project_first_part-PIC-II/llistabeguts.db')
-        #cursor = conn.execute("SELECT id, quantitatbeguda from beguts where id=?",(self.data2,))
-        #self.data = [row for row in cursor]
-        #print "aixo es data"
-        #print self.data
-
-
-
-        #cursor = conn.execute("SELECT username, email from users where (username,email) values (?,?)",username, email )
-        #t = [e for e in cursor]
-
-
         print "printejo t = ", t
-
         if t != None:
-            self.data = [row for row in cursor1]
-            print "OK"
+            data = [row for row in cursor1]
+            print "AIXO ES DATA"
+            print data
+            iden=data[0]
+            for a in iden:
+                print a
+                tagID=a
+            #print "aixo es TAGID"
+            #print tagID
+            #print "AIXO ES iden"
+
+            #print iden
+            conn = sqlite3.connect('/home/josep/Project_first_part-PIC-II/llistabeguts.db')
+            cursor = conn.execute("SELECT id, quantitatbeguda from beguts where id=?",(tagID,))
+            data2 = [row for row in cursor]
+            print "AIXO ES DATA 2"
+            print data2
+            quantitat_beguda=data2[0]
+            for a in data2:
+                print "aixo es al",a
+                for b in a:
+                    print "aixo es b",b
+            self.TOTALBEGUT=b
+
+            #self.data = [row for row in cursor]
+            #print "aixo es data"
+            #print self.data
+
+            #cursor = conn.execute("SELECT username, email from users where (username,email) values (?,?)",username, email )
+            #t = [e for e in cursor]
+
+
+
+
+            conn.close()
+
+
+
 
             return True
         else:
-                return False
-        conn.close()
+            return False
 
-    def do(self, command):
-        print "Will play music  ", " ".join(command)
-        return "OK"
+
+    def do(self,user):
+
+        passaralcanal= str("El ") + str(user) + str(" ha begut ") + str(self.TOTALBEGUT) + str(" cl")
+        print passaralcanal
+        return passaralcanal
 
 #######################################################################
     def is_for_yo4u(self, word):
