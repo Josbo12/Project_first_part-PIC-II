63906097478abf41fb8ac74c94f08df58f9ea9fb
Josbo12 <josepborras8@gmail.com>
2016-05-02 19:43:31 +0200

    separa en dos funcions

--- nfckeg/sensors/FlowSensor.py
+++ nfckeg/sensors/FlowSensor.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 import time
 import sqlite3
 
@@ -13,70 +15,41 @@ class FlowSensor(Action):
 
 
     def setup(self,tagID):
-        #i=3
-        #self.quantitatbeguda=40
-        #abc=True
-        #print self.quantitatbeguda
-        #return True
-
-        #while abc==True:
-        #    delay = 5
-        #    values=[]
-#
-#            self.quantity_flow = i #sensor  #guarda les dades del sensor en una variable
-#            values.append(self.quantity_flow)
-#            time.sleep(3)       #segons entre lectura i lectura
-#            print self.quantity_flow
-#            i=i-1
-#            if i==0:
-#              break
-            #delay = 5
-            #self.values=list()
 
 
+            #while tagID != None:
+                self.quantity_flow = float(50) #sensor guarda les dades del sensor en una variable
+                #self.values.append(quantity_flow)
 
-            quantity_flow = float(50) #sensor  #guarda les dades del sensor en una variable
-            #self.values.append(quantity_flow)
-            #time.sleep(5)       #segons entre lectura i lectura
-            print quantity_flow
-            conn = sqlite3.connect('/home/josep/Project_first_part-PIC-II/llistabeguts.db')
-            cursor = conn.execute("SELECT id,quantitatbeguda from beguts where id=?" , (tagID,) )
-            exist_nfc=cursor.fetchone()
-
-
-            if exist_nfc == None:
-
-                conn.execute('insert into beguts (id, quantitatbeguda)' +
-                            'values (?, ?);',
-                            (tagID,quantity_flow,))
-                conn.commit()
-            else:
-                print cursor
-                data = [row for row in cursor]
-                #print "AIXO ES DATA"
-                print len(data)
-                id_quantitat=data[0]
-                for a in id_quantitat:
-                    print a
-                quantitatbeguda=float(a)
-                print "AIXO ES EL QUE HE BEGUT ABANS",a
-                print "begut ara", quantity_flow
-                self.TOTAL_begut= quantitatbeguda + quantity_flow
-                print "total begut  =", self.TOTAL_begut
-                conn.execute("UPDATE beguts set quantitatbeguda=? where id=?", (self.TOTAL_begut,tagID,))
-                conn.commit()
-                cursor = conn.execute("SELECT id, quantitatbeguda from beguts where id=?" , (tagID,))
-                data = [row for row in cursor]
-                print "data actualitzat",data
-
-
-
-            conn.close()
-            print "arribo"
-
+                #time.sleep(5)       #segons entre lectura i lectura
+                print " \nQuantitat beguda ara:",self.quantity_flow
+            # begut = self.values[len(self.values-1)]-self.values(0)  ultima quantita registrada
+                                                                    # menys la primera registrada
 
     def get_data(self, tagID):
-        pass
+
+
+        conn = sqlite3.connect('/home/josep/Project_first_part-PIC-II/llistabeguts.db')
+        cursor = conn.execute("SELECT id,quantitatbeguda from beguts where id=?" , (tagID,) )
+        exist_nfc=cursor.fetchone()
+
+        if exist_nfc == None:
+
+            conn.execute('insert into beguts (id, quantitatbeguda)' +
+                        'values (?, ?);',
+                        (tagID,self.quantity_flow,))
+            conn.commit()
+        else:
+            data = [row for row in cursor]
+            id_quantitat=data[0]
+            for a in id_quantitat:
+                print ""
+            quantitatbeguda=float(a)
+            Total_begut= quantitatbeguda + self.quantity_flow
+            print "En total has begut: ", Total_begut
+            conn.execute("UPDATE beguts set quantitatbeguda=? where id=?", (Total_begut,tagID,))
+            conn.commit()
+        conn.close()
 
 
     def  get_cumulative():
