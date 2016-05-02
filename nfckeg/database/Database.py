5a9bbfd2800c83186d8715a209dfcaefccc8df4c
Josbo12 <josepborras8@gmail.com>
2016-05-02 19:43:45 +0200

    treure prints

--- nfckeg/database/Database.py
+++ nfckeg/database/Database.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 import sys
 import sqlite3
 from Action import Action
@@ -10,11 +12,8 @@ class Database(Action):
          super(Database, self).__init__(cfg, name)
 
     def save_data(self, cfg=None):
-
+        print "\n"
         conn = sqlite3.connect('/home/josep/Project_first_part-PIC-II/database.db')
-
-
-
         users = self.cfg["database"]["user"]
         for user in users:
             for usn in user:
@@ -24,31 +23,30 @@ class Database(Action):
                 cursor = conn.execute("SELECT user, nfc, username from users where  user=? and nfc=? and username=?" ,(usn,nfc,username))
                 t=cursor.fetchone()
                 if t == None:
-                    print "USUARIS AFEGITS:"
+                    print "USUARI AFEGIT:"
                     print usn, username, nfc
                     conn.execute('insert into users (user,nfc,username)' +
                                 'values (?, ?, ?);',
                                 (usn, nfc, username))
                 else:
-                    print "USUARIS NO AFEGITS"
+                    print "USUARI NO AFEGIT PERQUE JA EXISTEIX "
                     print usn, username, nfc
+                    print "\n"
 
-        #user = user.strip()
-        #nfc = self.cfg["database"]["nfc"]
-        #username = self.cfg["database"]["username"]
-        #conn.execute('insert into users (user,nfc,username)' +
-        #            'values (?, ?, ?);',
-        #            (user, nfc, username))
-        #print ">%s<" %(nfc)
         conn.commit()
         conn.close()
-
-
 
     def get_data(self):
         conn = sqlite3.connect('/home/josep/Project_first_part-PIC-II/database.db')
         cursor = conn.execute("SELECT user,nfc,username from users")
         data = [row for row in cursor]
-        #print data
+        conn.close()
+        return data
+
+    def set_data(self, command):
+        conn = sqlite3.connect('/home/josep/Project_first_part-PIC-II/database.db')
+        cursor = conn.execute("UPDATE database set nfc=? where user=?", (user,))
+        conn.commit()
+        data = [row for row in cursor]
         conn.close()
         return data
